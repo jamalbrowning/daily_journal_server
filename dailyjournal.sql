a8305a24847d4bd3392cf8286d19c70233a1008a
Jim Browning <jamalebrowning@gmail.com>
2020-10-16 18:30:11 -0500

    redo data

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `Entries` (
-        `id`,
-        `concept`,
-        `entry`,
-        `date`,
-        `moodId`
+        `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        `concept` TEXT NOT NULL,
+        `entry` TEXT NOT NULL,
+        `date` INTEGER NOT NULL,
+        FOREIGN KEY(`moodId`) REFERENCES `Moods`(`id`)
 );
 
 CREATE TABLE `Moods` (
-        `id`,
-        `label`
+  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `label` TEXT NOT NULL
 );
 
 
