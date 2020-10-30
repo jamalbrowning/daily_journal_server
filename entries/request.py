8f877148fcc5cc90aae71ee0f996c5092003ddd1
Jim Browning <jamalebrowning@gmail.com>
2020-10-29 19:24:11 -0500

    add entry

--- entries/request.py
+++ entries/request.py
@@ -102,3 +102,28 @@ def delete_entry(id):
         DELETE FROM entries
         WHERE id = ?
         """, (id, ))
+
+def new_journal_entry(new_entry):
+    with sqlite3.connect("./dailyjournal.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO entries
+            ( concept, entry, date, moodId )
+        VALUES
+            ( ?, ?, ?, ?);
+        """, (new_entry['concept'],new_entry['entry'],
+              new_entry['date'],new_entry['moodId'], ))
+
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
+
+        # Add the `id` property to the entry dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_entry['id'] = id
+
+
+    return json.dumps(new_entry)
